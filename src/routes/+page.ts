f134df3067a853c2293d579c4677da6f8628d407
unknown <tyuplus5@gmail.com>
2023-12-08 17:28:13 +0900

    second commit

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,1 +1,9 @@
-export const prerender = true;
+import type { PageLoad } from "./$types"
+
+export const load: PageLoad = (data) => {
+    return {
+        user: data.data.user as any,
+        challenge: data.data.challenge as any,
+        hasTree: data.data.hasTree
+    }
+}
